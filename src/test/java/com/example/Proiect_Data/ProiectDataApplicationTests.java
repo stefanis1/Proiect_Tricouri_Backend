182e050258bb4e78329c9576a22233bc10b91e0b
stefanis1 <105522507+stefanis1@users.noreply.github.com>
2025-11-17 00:37:08 +0200

    Prima versiune

---
+++ src/test/java/com/example/Proiect_Data/ProiectDataApplicationTests.java
@@ -0,0 +1,19 @@
+package com.example.Proiect_Data;
+
+import com.example.Proiect_Data.Domain.Tricou;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProiectDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+//        Tricou e1 = new Tricou(10,"FcBarcelona","S",10,2,140);
+//        Tricou e2=new Tricou(11,"FcRealMadrid","S",20,1,140);
+//        Service1 serviceTricou=new Service1();
+//        serviceTricou.add_Tricou(e1);
+//        assert (serviceTricou.size()==1);
+    }
+
+}
---
+++ src/test/java/com/example/Proiect_Data/test.java
@@ -0,0 +1,7 @@
+package com.example.Proiect_Data;
+
+import java.util.ArrayList;
+
+public class test {
+
+}
